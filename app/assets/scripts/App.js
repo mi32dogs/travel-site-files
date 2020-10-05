355532c7fe8f92ddccfeb531bf8a2981dc3149f8
rob <rob@1stdesign.net>
2020-10-05 10:42:00 -0700

    completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,11 +2,13 @@ import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
+import Modal from "./modules/Modal"
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 let mobileMenu = new MobileMenu()
 let stickyHeader = new StickyHeader()
+let modal = new Modal()
 
 if (module.hot) {
   module.hot.accept()
